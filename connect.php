544fbf4464640811010e880c3499fcace8f81a96
anamika111 <anamika.sanap97@gmail.com>
2020-01-17 17:48:24 +0530

    Using PDO Method

--- connect.php
+++ connect.php
@@ -1,7 +1,13 @@
 <?php
 require 'db.php';
 $db=new DB();
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+exit();
   $is_inserted=  $db->insert('information',$_POST);
+
 
   if($is_inserted){
 //      header("location: experiment.local/listing.php");
