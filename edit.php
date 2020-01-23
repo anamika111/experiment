6e9050d5e81b5b01fe5e67d406bd1d3f4c523edc
anamika111 <anamika.sanap97@gmail.com>
2020-01-23 15:16:37 +0530

    validation

--- edit.php
+++ edit.php
@@ -29,31 +29,38 @@ if (isset($_POST['email'])) {
         $error['email'] = "Email field should not be empty";
     } else {
         $matches = array();
-        preg_match_all('(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,3})$)', $_POST['email'], $matches);
+        preg_match_all('(^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$)', $_POST['email'], $matches);
         if (empty($matches[0])) {
             $error['email'] = "Please enter valid email address (abc@gmail.com)";
 
         }
     }
 }
-//$num[0] != 9 && $num[0] != 8 && $num[0] != 7))
-if(isset($_POST['contact'])) {
-$num=$_POST['contact'];
-if((strlen($num)!=10)) {
 
-    $error['contact'] = "Please enter only number & number should be 10 digit";
+if (isset($_POST['contact'])) {
+    if (empty($_POST['contact'])) {
+        $error['contact'] = "Address field should not be empty";
+    } else {
+        preg_match_all('(^[6789]\d{9}$)', $_POST['contact'], $matches);
+        if (empty($matches[0])) {
+            $error['contact'] = "Please Enter valid contact Number ";
 
-}
-elseif($num[0]!=9 && $num[0]!=8 && $num[0]!=7 && $num[0]!=6 ){
-    $error['contact'] = "Please enter only number & number Should start with 9 8 7 6";
-}
-
-
-
-
-
+        }
+    }
 
 }
+//if(isset($_POST['contact'])) {
+//    $num = $_POST['contact'];
+//    if ((strlen($num) != 10)) {
+//
+//        $error['contact'] = "Please enter only number & number should be 10 digit";
+//
+//    } elseif ($num[0] != 9 && $num[0] != 8 && $num[0] != 7 && $num[0] != 6) {
+//        $error['contact'] = "Please enter only number & number Should start with 9 8 7 6";
+//    }
+//}
+
+
 if (isset($_POST['address'])) {
     if (empty($_POST['address'])) {
         $error['address'] = "Address field should not be empty";
@@ -67,12 +74,15 @@ if (isset($_POST['address'])) {
 
 }
 
+
+
 if ($_POST && empty($error))
 {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $contact = $_POST['contact'];
     $address = $_POST['address'];
+
 
 
     $db->update('information', $_POST, " id='$id'");
@@ -218,8 +228,6 @@ $res = $res[0];
 
 
 
-
-
                 <div class="form-group">
                     <label for="address"><b>Address</b></label>
                     <input class="form-control" type="text" placeholder="Enter address" name="address"
@@ -252,3 +260,7 @@ $res = $res[0];
 
 </body>
 </html>
+
+
+<!--Dob-([0-9]{2})\/([0-9]{2})\/([0-9]{4})-->
+<!--mobile no-^[6789]\d{9}$-->
